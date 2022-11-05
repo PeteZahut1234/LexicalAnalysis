a7693c100574e4d0b78db19f193717d8dd9e8e58
Christian <louisjohnson@protonmail.com>
2022-11-04 21:39:52 -0600

    Modifications and new features but is not working yet

--- src/codigo/Tokens.java
+++ src/codigo/Tokens.java
@@ -9,12 +9,22 @@ package codigo;
  * @author Christian
  */
 public enum Tokens {
-    Reservadas,
+    Linea,
+    Int,
+    If,
+    Else,
+    While,
     Igual,
     Suma,
     Resta,
     Multiplicacion,
     Division,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Main,
+    P_coma,
     Identificador,
     Numero,
     ERROR
